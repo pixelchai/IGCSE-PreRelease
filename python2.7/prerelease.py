31a89f5bdd1c72d290dd5b86d749ff5d32ff5a1b
syanochara <sianochara@gmail.com>
2018-03-09 22:05:52 +0000

    implementing python2.7 dictionary version

--- python2.7/prerelease.py
+++ python2.7/prerelease.py
@@ -16,22 +16,48 @@ components={
             '19"':[65,10],
             '23"': [120, 10],
     },
-    'Case':{
+    'case':{
             'Mini Tower':[40,10],
             'Midi Tower': [70, 10],
     },
-    'USB ports':{
+    'usb ports':{
             '2 ports':[10,10],
             '4 ports': [20, 10],
     }
 }
+ordernumbers=[]#is explicitly required in task 2
+orderno = 1
+while True:
+    choices={}
+    #choices
+    for component in components:
+        print("Please select your desired "+component+":")
+        opts = components[component].keys()
+        i=0
+        for opt in opts:
+            i+=1
+            print(str(i)+") "+opt +" - $"+str(components[component][opt][0]))
 
-#task 1 - produce an estimate
-choices=[]
-#choices
-for component in components:
-    print("Please enter your desired "+component+" no:")
-    i=1
-    for opt in components[component]:
-       print(str(i)+") "+opt)
-       i+=1
+        while True:
+            input=raw_input()
+            if(not input.isdigit()):
+                print("Input was not digits only. Please enter a number (1-"+str(i)+"). Please try again.")
+                continue
+            try:
+                selection = int(input)
+                if(selection<1 or selection>i):
+                    print("Selection out of range. Please enter a number (1-" + str(i) + "). Please try again.")
+                    continue
+            except:
+                #this theoretically should never happen
+                print("Internal error parsing input. Please try again.")
+                continue
+            print str(i)
+            choices[component]=opts[selection-1]
+            break
+
+    print "Estimation:"
+    #todo
+
+    if raw_input("Another order? (y/n)\n").lower()=='n': break
+    orderno+=1
