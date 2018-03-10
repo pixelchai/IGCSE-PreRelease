eff13f294fe6de411409967924705f8c9e59ad9c
syanochara <sianochara@gmail.com>
2018-03-10 00:17:15 +0000

    refining (did some beta testing with various people...)

--- python2.7/prerelease.py
+++ python2.7/prerelease.py
@@ -7,29 +7,29 @@ import datetime
 # -> [2] = no. sold
 components={
     'processor':{
-        'p3':[100,10, 0],
-        'p5': [120, 10, 0],
-        'p7': [200, 10, 0],
+        'p3':[100, 2, 0],
+        'p5': [120, 1, 0],
+        'p7': [200, 3, 0],
     },
     'ram':{
-            '16GB':[75, 10, 0],
-            '32GB': [150, 10, 0],
+            '16GB':[75, 2, 0],
+            '32GB': [150, 1, 0],
     },
     'storage':{
-            '1TB':[50, 10, 0],
-            '2TB': [100, 10, 0],
+            '1TB':[50, 2, 0],
+            '2TB': [100, 4, 0],
     },
     'screen':{
-            '19"':[65, 10, 0],
-            '23"': [120, 10, 0],
+            '19"':[65, 1, 0],
+            '23"': [120, 2, 0],
     },
     'case':{
-            'Mini Tower':[40, 10, 0],
-            'Midi Tower': [70, 10, 0],
+            'Mini Tower':[40, 1, 0],
+            'Midi Tower': [70, 2, 0],
     },
     'usb ports':{
-            '2 ports':[10, 10, 0],
-            '4 ports': [20, 10, 0],
+            '2 ports':[10, 1, 0],
+            '4 ports': [20, 2, 0],
     }
 }
 
@@ -47,28 +47,28 @@ def getChoice(prompt="Yes or no?"):
 
 ordernumbers=[]#is explicitly required in task 2
 estimateno = 1
-sum=0
+saleslist=[]
 done = False
 while not done:
     choices={}
     #choices
     for component in components:
-        print("Please select your desired "+component+": ")
         opts = components[component].keys()
-        i=0
+        print("Please select your desired "+component+" (1-"+str(len(opts))+"): ")
+        counter=0
         for opt in opts:
-            i+=1
-            print(str(i)+") "+opt +" - $"+str(components[component][opt][0]))
+            counter+=1
+            print(str(counter) + ") " + opt + " - $" + str(components[component][opt][0]) + " - " + str(components[component][opt][1]) + " in stock.") #does not explicitly say to show stock in task but impl anyway
 
         while True:
             input=raw_input()
             if(not input.isdigit()):
-                print("Input was not digits only. Please enter a number (1-"+str(i)+"). Please try again.")
+                print("Please input digits only. Please enter a number (1-" + str(counter) + "). Please try again.")
                 continue
             try:
                 selection = int(input)
-                if(selection<1 or selection>i):
-                    print("Selection out of range. Please enter a number (1-" + str(i) + "). Please try again.")
+                if(selection<1 or selection>counter):
+                    print("Selection out of range. Please enter a number (1-" + str(counter) + "). Please try again.")
                     continue
             except:
                 #should theoretically never happen
@@ -109,7 +109,7 @@ while not done:
                 components[component][choices[component]][1]-=1
                 components[component][choices[component]][2] += 1 #need for task 3
             ordernumbers.append(estimateno) #'add the unique estimate number to the list of order numbers'
-            sum+= estimate #need for task 3
+            saleslist.append(estimate) #need for task 3
 
             #assuming 'add the customer's details' means add it to the order summary output (as opp. to add to a list?)
             #using string here so can be changed to physical print easily if need be (question ambiguous)
@@ -123,8 +123,8 @@ while not done:
             ordersummary+=("Customer's details: " + raw_input('Please enter your details.\n')+'\n') #because doesn't really specify what type of details
             ordersummary+=("Date: " + str(datetime.datetime.now().date())+'\n\n')
 
-            for i in range(2): #'print two copies' assuming print means print to console
-                print(("Customer Copy:" if i % 2 == 0 else "Shop Copy:")+ordersummary)
+            for counter in range(2): #'print two copies' assuming print means print to console
+                print(("Customer Copy:" if counter % 2 == 0 else "Shop Copy:") + ordersummary)
 
     print('')
     if not getChoice('Is there another order?'):
@@ -135,7 +135,13 @@ while not done:
 #task 3
 print("End of day Summary:")
 print("Total orders made: "+str(len(ordernumbers)))
-print("Total value of the orders: $"+str(sum)) #assuming 'showing the total (...) value of the orders' (ambiguous)
+sumsales = 0
+counter=1
+for sale in saleslist:
+    print("\tOrder #"+str(counter)+" - $"+str(sale))
+    sumsales+=sale
+    counter+=1
+print("Total value of the orders: $" + str(sumsales)) #assuming 'showing the total (...) value of the orders' (ambiguous)
 for component in components:
     print(component+" sales:")
     for type in components[component]:
