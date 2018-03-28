7f55e472c72a6af2d6d0244c32e36d74c97dabcf
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2018-03-28 08:32:18 +0000

    start implement ms version

---
+++ python2.7/prerelease_ms.py
@@ -0,0 +1,4 @@
+import datetime
+componentnames=['processor','ram','storage','screen','case','usb ports']
+sections=[3,2,2,2,2,2]
+componentoptions=['p3','p5','p7','16GB','32GB','1TB','2TB','19"','23"','Mini Tower','Midi Tower','2 ports','4 ports']
